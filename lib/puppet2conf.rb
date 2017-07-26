4424e72cbeaf688be47f8630401c08df7536093c
Eugene Piven <thepegasd@gmail.com>
2017-07-26 15:47:33 +0300

    Cleanup/lint

--- lib/puppet2conf.rb
+++ lib/puppet2conf.rb
@@ -1,4 +1,4 @@
-require "puppet2conf/version"
+require 'puppet2conf/version'
 require 'strings2conf'
 require 'md2conf'
 require 'confluence/api/client'
@@ -16,9 +16,9 @@ module Puppet2conf
     end
 
     def push_page(page_title, html, ancestor)
-      page = @client.get({ spaceKey: @space, title: page_title })[0]
+      page = @client.get(spaceKey: @space, title: page_title)[0]
       if ancestor
-        parent_page = @client.get({ spaceKey: @space, title: ancestor })[0]
+        parent_page = @client.get(spaceKey: @space, title: ancestor)[0]
         if parent_page
           ancestors = [{ type: 'page', id: parent_page['id'] }]
         else
@@ -28,39 +28,38 @@ module Puppet2conf
       end
       if page.nil?
         puts "Page '#{page_title}' doesn't exist, creating it"
-        @client.create({
-          type:      "page",
+        @client.create(
+          type:      'page',
           title:     page_title,
           space:     { key: @space },
           ancestors: ancestors,
-          body:      { storage: { value: html, representation: "storage" } }
-        })
+          body:      { storage: { value: html, representation: 'storage' } }
+        )
       else
         page = @client.get_by_id(page['id'])
         puts "Page '#{page_title}' exists. Updating it"
         version = page['version']['number'] || 1
-        @client.update(page['id'], {
-          type:      "page",
+        @client.update(
+          age['id'],
+          type:      'page',
           id:        page['id'],
           title:     page_title,
           space:     { key: @space },
           ancestors: ancestors,
           version:   { number: version + 1 },
-          body:      { storage: { value: html, representation: "storage" } }
-        })
+          body:      { storage: { value: html, representation: 'storage' } }
+        )
       end
     end
 
-
-    def gendocs(module_name, path='./', pushstrings=false)
+    def gendocs(module_name, path = './', pushstrings = false)
       module_html = Md2conf.parse_markdown File.read(path + 'README.md')
       push_page(module_name, module_html, @ancestor)
       Dir.glob(path + '*.md').each do |md_file|
-        unless md_file.end_with? 'README.md'
-          html       = Md2conf.parse_markdown File.read(md_file)
-          page_title = "#{module_name} #{File.basename(md_file).sub('.md', '')}"
-          push_page(page_title, html, module_name)
-        end
+        next if md_file.end_with? 'README.md'
+        html       = Md2conf.parse_markdown File.read(md_file)
+        page_title = "#{module_name} #{File.basename(md_file).sub('.md', '')}"
+        push_page(page_title, html, module_name)
       end
       if pushstrings
         PuppetStrings.generate(PuppetStrings::DEFAULT_SEARCH_PATTERNS, json: "#{path}#{module_name}.json")
@@ -68,6 +67,5 @@ module Puppet2conf
         push_page("#{module_name} Reference", reference_html, module_name)
       end
     end
-
   end
 end
